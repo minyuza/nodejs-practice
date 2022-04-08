2a080a1cefda93dea1f32f9cf6c0c9e09e93bc52
MINYUZA <sncr_crmn@naver.com>
2022-04-08 20:49:59 +0900

    DOM을 이용한 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("hi");
+const id = document.querySelector("#id");
+const password = document.querySelector("#id");
+const button = document.querySelector("button");
+
+button.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
