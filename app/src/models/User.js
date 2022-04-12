70ea95c17ec84f451172f2a2eba327b33183505b
MINYUZA <sncr_crmn@naver.com>
2022-04-12 22:52:58 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,15 +7,21 @@ class User {
   }
 
   login() {
-    const body = this.body;
-    const { id, password } = UserStorage.getUserInfo(body.id);
+    const client = this.body;
+    const { id, password } = UserStorage.getUserInfo(client.id);
     if (id) {
-      if (id === body.id && password === body.password) {
+      if (id === client.id && password === client.password) {
         return { success: true };
       }
       return { success: false, message: "비번 틀림" };
     }
     return { success: false, message: "아이디 존재 X" };
+  }
+
+  register() {
+    const client = this.body;
+    const response = UserStorage.save(client);
+    return response;
   }
 }
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,14 @@ const button = document.querySelector("#button");
 button.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주세요.");
+  if (password !== confirmPassword) {
+    return alert("비밀번호 일치X");
+  }
   const req = {
     id: id.value,
     password: password.value,
     name: name.value,
-    confirmPassword: confirmPassword.value,
   };
 
   fetch("/register", {
